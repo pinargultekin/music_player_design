0580f2aea36a6b8c697fd9dcad8458eb8b16b438
Pinar <pinargultekin@yahoo.com>
2020-09-09 17:38:25 -0400

    progress bar changed

--- src/PlayBar.jsx
+++ src/PlayBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import ProgressBar from 'react-bootstrap/ProgressBar';
+// import ProgressBar from 'react-bootstrap/ProgressBar';
 import "./styles.css";
 
 function PlayBar(){
 return <div className="progressbar">
-<ProgressBar variant="warning" animated now={45}  />
+<div className="progress"> </div>
+{/* <ProgressBar variant="warning" animated now={45}  /> */}
 
 </div>
 
